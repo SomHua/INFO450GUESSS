b957aedf7c6252db79018895f1eb64a28ea02897
Som Hua <Som@soms-macbook-pro.local>
2018-10-21 21:42:27 -0400

    Initial Commit

---
+++ INFO450GUESS/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  INFO450GUESS
+//
+//  Created by Som Hua on 10/21/18.
+//  Copyright © 2018 Som Hua. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
